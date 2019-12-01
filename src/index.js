4b8eb69653d3dfd3aff54a068844bf3fb4e1be19
Elaasemy <hamzaelaasemy@gmail.com>
2019-12-02 00:24:49 +0100

    creating login  and  register (not  working)

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+
 import registerServiceWorker from "./registerServiceWorker";
+import "semantic-ui-css/semantic.min.css";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const Root = () => (
   <Router>
     <Switch>
-      <Route path="/" component={App} />
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </Switch>
   </Router>
 );
